e4ff3c7746d0a96d69ab791185a0ede25155e997
FoldeR-ua <61949378+FoldeR-ua@users.noreply.github.com>
2022-01-27 11:30:08 +0200

    Add project files.

---
+++ MusApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MusApp.Parsing;
+using HtmlAgilityPack;
+namespace MusApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        class Item
+        {
+            public string Number { get; set; }
+            public string Title { get; set; }
+            public string Artist { get; set; }
+        }
+
+        private void appearContent(object sender, EventArgs e)
+        {
+            ParsingMethod parsing = new ParsingMethod();
+            var temp = parsing.ParsingNodes();
+            var mus = temp.title;
+            var artists = temp.artist;
+            List<Item> items = new List<Item>();
+
+            for (int i = 0; i < mus.Count; i++)
+            {
+                items.Add(new Item { Number = i+1 + ".", Title = mus[i].InnerText, Artist = artists[i].InnerText });
+            }
+
+            songList.ItemsSource = items;
+
+        }
+    }
+}
---
+++ MusApp/ParsingFolder/ParsingMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace MusApp.Parsing
+{
+    class ParsingMethod
+    {
+        private string url = "https://www.traxsource.com/title/1731185/-weekend-chart-26--last-of-the-year";
+        public string GetUrl { get; }
+
+        public (HtmlNodeCollection artist, HtmlNodeCollection title) ParsingNodes()
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(this.url);
+            var titles = doc.DocumentNode.SelectNodes("//div[@class='trk-cell title']");
+            var artist = doc.DocumentNode.SelectNodes("//div[@class='trk-cell artists']");
+
+            return (artist, titles);
+        }
+     }
+}
